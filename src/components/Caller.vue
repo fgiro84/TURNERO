472aa78eef98f93d5a97038b402732159c2b74f2
fgiro84 <fgiro841107@gmail.com>
2025-02-18 14:29:03 -0300

    18022025

--- src/components/Caller.vue
+++ src/components/Caller.vue
@@ -1,114 +1,110 @@
 <template>
   <v-main style="background-color: white;">
-    <v-container class="pa-4" sm="12" md="8" style="background-color: white;">
-      <v-row justify="center" align="center" dense>
-        <!-- Primera columna -->
-        <v-col cols="12" sm="6" md="4" class="d-flex align-stretch">
-          <v-card class="flex-grow-1 d-flex justify-center align-center" color="#1b488e" dark
-            style="min-height: 100px;">
-            <v-card-title class="text-h5 text-center text-white">FILA MÁXIMO<br>15 UNIDADES</v-card-title>
-          </v-card>
-        </v-col>
-        <!-- Segunda columna -->
-        <v-col cols="12" sm="6" md="4" class="d-flex align-stretch">
-          <v-card class="flex-grow-1 d-flex justify-center align-center" color="#52c8e1" dark
-            style="min-height: 100px;">
-            <v-card-title class="text-h5 text-center text-white">FILA SIN LÍMITE<br>DE UNIDADES</v-card-title>
-          </v-card>
-        </v-col>
-        <!-- Tercera columna -->
-        <v-col cols="12" sm="6" md="4" class="d-flex align-stretch">
-          <v-card class="flex-grow-1 d-flex justify-center align-center" color="#ee7a20" dark
-            style="min-height: 100px;">
-            <v-card-title class="text-h5 text-center text-white">FILA PRIORIDAD</v-card-title>
-          </v-card>
-        </v-col>
-      </v-row>
+    <v-container   style="background-color: white;">
+      <v-row style="margin-top: 0px;" > <!-- Añadimos la clase gap-4 para separación uniforme entre las columnas -->
+        <!-- COL IZQUIERDA -->
+        <v-col cols="12" md="4" justify="center" align="start" dense class="px-5 " >
+          <!-- FILA 1 -->
+          <v-row class="mb-4">
+            <v-card class="flex-grow-1 d-flex justify-center align-center" color="#1b488e" dark
+              style="min-height: 95px;">
+              <v-card-title class="text-h5 text-center text-white">FILA MÁXIMO<br>15 UNIDADES</v-card-title>
+            </v-card>
+          </v-row>
 
-      <v-row justify="center" align="center" dense>
-        <!-- Primera columna -->
-        <v-col cols="12" sm="6" md="4" class="d-flex align-stretch">
-          <v-card :class="{ 'custom-red': caja1 === 0 }"
-            class="flex-grow-1 d-flex flex-column justify-center align-center" color="#8bc63e" dark
-            style="min-height: 240px;">
-            <v-card-title class="text-h5 text-center text-white" style="margin-bottom: 0;">
-              {{ caja1 === 0 ? '' : 'CAJA' }}
-            </v-card-title>
-
-            <div v-if="caja1 === 0" class="text-center text-white" style="font-size: 30px; font-weight: bold;">
-              LAS CAJAS<br />SE ENCUENTRAN<br />OCUPADAS
-            </div>
-            <div v-else class="text-center text-white" style="font-size: 90px; font-weight: bold;">
-              {{ caja1 }}
-            </div>
-
-            <v-card-title class="text-h5 text-center text-white" style="margin-top: 0;">
-              {{ caja1 === 0 ? '' : 'DISPONIBLE' }}
-            </v-card-title>
-          </v-card>
-        </v-col>
-        <!-- Segunda columna -->
-        <v-col cols="12" sm="6" md="4" class="d-flex align-stretch">
-          <v-card :class="{ 'custom-red': caja2 === 0 }"
-            class="flex-grow-1 d-flex flex-column justify-center align-center" color="#8bc63e" dark
-            style="min-height: 240px;">
-            <v-card-title class="text-h5 text-center text-white" style="margin-bottom: 0;">
-              {{ caja2 === 0 ? '' : 'CAJA' }}
-            </v-card-title>
-
-            <div v-if="caja2 === 0" class="text-center text-white" style="font-size: 30px; font-weight: bold;">
-              LAS CAJAS<br />SE ENCUENTRAN<br />OCUPADAS
-            </div>
-            <div v-else class="text-center text-white" style="font-size: 90px; font-weight: bold;">
-              {{ caja2 }}
-            </div>
-
-            <v-card-title class="text-h5 text-center text-white" style="margin-top: 0;">
-              {{ caja2 === 0 ? '' : 'DISPONIBLE' }}
-            </v-card-title>
-          </v-card>
+          <!-- CAJA 1 -->
+          <v-row class="mb-2">
+            <v-card :class="{ 'custom-red': caja1 === 0 }"
+              class="flex-grow-1 d-flex flex-column justify-center align-center" color="#8bc63e" dark
+              style="min-height: 245px;">
+              <v-card-title class="text-h5 text-center text-white">{{ caja1 === 0 ? '' : 'CAJA' }}</v-card-title>
+              <div v-if="caja1 === 0" class="text-center text-white" style="font-size: 30px; font-weight: bold;">
+                LAS CAJAS<br />SE ENCUENTRAN<br />OCUPADAS
+              </div>
+              <div v-else class="text-center text-white" style="font-size: 90px; font-weight: bold;">
+                {{ caja1 }}
+              </div>
+              <v-card-title class="text-h5 text-center text-white">{{ caja1 === 0 ? '' : 'DISPONIBLE' }}</v-card-title>
+            </v-card>
+          </v-row>
         </v-col>
 
-        <!-- Tercera columna -->
-        <v-col cols="12" sm="6" md="4" class="d-flex align-stretch">
-          <v-card :class="{ 'custom-red': caja3 === 0 }"
-            class="flex-grow-1 d-flex flex-column justify-center align-center" color="#8bc63e" dark
-            style="min-height: 240px;">
-            <v-card-title class="text-h5 text-center text-white" style="margin-bottom: 0;">
-              {{ caja3 === 0 ? '' : 'CAJA' }}
-            </v-card-title>
+        <!-- COL CENTRO -->
+        <v-col cols="12" md="4" justify="center" align="start" dense class="px-5">
+          <!-- FILA 1 -->
+          <v-row class="mb-4">
+            <v-card class="flex-grow-1 d-flex justify-center align-center" color="#52c8e1" dark
+              style="min-height: 95px;">
+              <v-card-title class="text-h5 text-center text-white">FILA SIN LÍMITE<br>DE UNIDADES</v-card-title>
+            </v-card>
+          </v-row>
 
-            <div v-if="caja3 === 0" class="text-center text-white" style="font-size: 30px; font-weight: bold;">
-              LAS CAJAS<br />SE ENCUENTRAN<br />OCUPADAS
-            </div>
-            <div v-else class="text-center text-white" style="font-size: 90px; font-weight: bold;">
-              {{ caja3 }}
-            </div>
+          <!-- CAJA 2 -->
+          <v-row class="mb-2">
+            <v-card :class="{ 'custom-red': caja2 === 0 }"
+              class="flex-grow-1 d-flex flex-column justify-center align-center" color="#8bc63e" dark
+              style="min-height: 245px;">
+              <v-card-title class="text-h5 text-center text-white">{{ caja2 === 0 ? '' : 'CAJA' }}</v-card-title>
+              <div v-if="caja2 === 0" class="text-center text-white" style="font-size: 30px; font-weight: bold;">
+                LAS CAJAS<br />SE ENCUENTRAN<br />OCUPADAS
+              </div>
+              <div v-else class="text-center text-white" style="font-size: 90px; font-weight: bold;">
+                {{ caja2 }}
+              </div>
+              <v-card-title class="text-h5 text-center text-white">{{ caja2 === 0 ? '' : 'DISPONIBLE' }}</v-card-title>
+            </v-card>
+          </v-row>
+        </v-col>
 
-            <v-card-title class="text-h5 text-center text-white" style="margin-top: 0;">
-              {{ caja3 === 0 ? '' : 'DISPONIBLE' }}
-            </v-card-title>
-          </v-card>
+        <!-- COL DERECHA -->
+        <v-col cols="12" md="4" justify="center" align="start" dense class="px-5">
+          <!-- FILA 1 -->
+          <v-row class="mb-4">
+            <v-card class="flex-grow-1 d-flex justify-center align-center" color="#ee7a20" dark
+              style="min-height: 95px;">
+              <v-card-title class="text-h5 text-center text-white">FILA PRIORIDAD</v-card-title>
+            </v-card>
+          </v-row>
+
+          <!-- CAJA 3 -->
+          <v-row class="mb-2">
+            <v-card :class="{ 'custom-red': caja3 === 0 }"
+              class="flex-grow-1 d-flex flex-column justify-center align-center" color="#8bc63e" dark
+              style="min-height: 245px;">
+              <v-card-title class="text-h5 text-center text-white">{{ caja3 === 0 ? '' : 'CAJA' }}</v-card-title>
+              <div v-if="caja3 === 0" class="text-center text-white" style="font-size: 30px; font-weight: bold;">
+                LAS CAJAS<br />SE ENCUENTRAN<br />OCUPADAS
+              </div>
+              <div v-else class="text-center text-white" style="font-size: 90px; font-weight: bold;">
+                {{ caja3 }}
+              </div>
+              <v-card-title class="text-h5 text-center text-white">{{ caja3 === 0 ? '' : 'DISPONIBLE' }}</v-card-title>
+            </v-card>
+          </v-row>
         </v-col>
       </v-row>
 
-      <v-row justify="center" align="center" dense>
-        <!-- Primera columna -->
-        <v-col cols="12" sm="12" md="12" class="d-flex align-stretch">
-          <v-card class="flex-grow-1 d-flex align-center" color="#b1e2f9" dark style="min-height: 100px;">
-            <v-card-title class="text-h5 text-center" style="color: #025aaa; font-size: 2rem; flex: 1; font-weight: bold;">
+      <!-- Mensaje e imagen -->
+      <v-row class="px-2"> <!-- Añadimos gap-4 para separar uniformemente este bloque -->
+        <v-card class="flex-grow-1 d-flex align-center" color="#b1e2f9" dark style="min-height: 100px;">
+          <v-row class="d-flex align-center justify-center flex-wrap">
+            <!-- Texto oculto en pantallas pequeñas y visible en pantallas medianas y grandes -->
+            <v-card-title class="text-h5 text-center d-none d-md-flex"
+              style="color: #025aaa; font-size: 2rem; font-weight: bold; margin-left: 70px;">
               Dos clientes por caja optimiza tu tiempo. Por eso, cuando veas un<br>espacio libre en la caja, podés pasar
               a descargar los productos.
             </v-card-title>
+
+            <!-- Imagen centrada en pantallas pequeñas y alineada a la derecha en pantallas grandes -->
             <img src="/demofoot.png" alt="Imagen de caja"
-              style="width: 180px; height: auto; align-self: flex-end; margin-left: 0px; margin-right: 50px;">
-          </v-card>
-
-        </v-col>
+              style="width: 180px; height: auto; margin-left: 0px; margin-right: 50px;"
+              class="d-block mx-auto">
+          </v-row>
+        </v-card>
       </v-row>
-
     </v-container>
   </v-main>
+
 </template>
 
 
